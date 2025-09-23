8cb43def2f242276625611f8325cf7ed34f4fb7f
Kanky9 <bournotsantino03@gmail.com>
2025-09-23 15:49:24 -0300

    fix bugs

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,10 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+// // Ajusta --vh para móviles. Ejecutar en load y resize.
+// const setVh = () => {
+//   document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
+// };
+// setVh();
+// window.addEventListener('resize', setVh);
+// // Ajusta --vh para móviles. Ejecutar en load y resize.
